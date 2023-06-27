1c25d62af6d071eebce512963eef6f2c333c7369
Gyuri Horak <dyuri@horak.hu>
2023-06-27 13:58:19 +0200

    performance comparison htmx vs lightweight json

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -7,6 +7,82 @@ const cardWidget = (card) => {
   `;
 };
 
+const fillForm = (form, data) => {
+  const keys = Object.keys(data);
+  keys.forEach((key) => {
+    const input = form.querySelector(`[name="${key}"]`);
+    if (input) {
+      input.value = data[key];
+    }
+  });
+};
+
+const STATE = {
+  startTime: 0
+};
+
+const autoRunHtmx = () => {
+  const form = document.getElementById('cardform');
+  const nrleft = +form.times.value;
+
+  if (nrleft > 0) {
+    const data = {
+      name: `John ${nrleft} Htmx`,
+      email: `john.${nrleft}@htmx.com`,
+      times: nrleft - 1,
+    };
+
+    fillForm(form, data);
+    const button = document.getElementById('card-button-htmx');
+    button.click();
+
+    document.addEventListener('htmx:afterSwap', () => {
+      autoRunHtmx();
+    }, { once: true });
+  } else {
+    const endTime = +new Date();
+    const duration = endTime - STATE.startTime;
+    const durationEl = document.getElementById('autorun-result');
+    durationEl.innerHTML = `${duration} ms`;
+  }
+};
+
+const autoRunJson = () => {
+  const form = document.getElementById('cardform');
+  const nrleft = +form.times.value;
+
+  if (nrleft > 0) {
+    const data = {
+      name: `John ${nrleft} Json`,
+      email: `john.${nrleft}@json.com`,
+      times: nrleft - 1,
+    };
+
+    fillForm(form, data);
+    const button = document.getElementById('card-button-json');
+    button.click();
+
+    document.addEventListener('json:ready', () => {
+      autoRunJson();
+    }, { once: true });
+  } else {
+    const endTime = +new Date();
+    const duration = endTime - STATE.startTime;
+    const durationEl = document.getElementById('autorun-result');
+    durationEl.innerHTML = `${duration} ms`;
+  }
+};
+
+const autoStartHtmx = () => {
+  STATE.startTime = +new Date();
+  autoRunHtmx();
+};
+
+const autoStartJson = () => {
+  STATE.startTime = +new Date();
+  autoRunJson();
+};
+
 document.addEventListener('click', (e) => {
   if (e.target.matches('[dy-post], [dy-post] *')) {
     e.preventDefault();
@@ -27,10 +103,14 @@ document.addEventListener('click', (e) => {
     }).then((data) => {
       const cardCnt = document.getElementById('card_cnt');
       cardCnt.innerHTML = cardWidget(data);
+
+      const event = new CustomEvent('json:ready', { bubbles: true });
+      document.dispatchEvent(event);
     });
+  } else if (e.target.matches('#autorun-htmx, #autorun-htmx *')) {
+    autoStartHtmx();
+  } else if (e.target.matches('#autorun-json, #autorun-json *')) {
+    autoStartJson();
   }
 });
 
-document.addEventListener('htmx:afterSettle', () => {
-  console.log('htmx:afterSettle');
-});
